931959d1c9e0730f04c734cc1818018dad72f109
MAHHARIUS <vinnikov2009@yandex.ru>
2017-08-25 15:22:37 +0300

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+
+    public static void main(String[] args) {
+	    int var1; // This declares a variable
+	    int var2; // This declares a variable
+        int var3;
+
+        var1 = 1024;
+        var2 = 500;
+        var3 = var1 + var2;
+
+        System.out.println("The sum of variables is " + var3 );
+
+    }
+}
